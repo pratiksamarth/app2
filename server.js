9bdf4e2d3879016aed64f016ded801814b2975d7
pratiksamarth <pratiksamarth296@gmail.com>
2019-05-29 15:59:56 +0530

    modified

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+
 
 app.listen(3000, '0.0.0.0', () => {
     console.log('server started on port 3000');
