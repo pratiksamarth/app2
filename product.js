f0a7cd3dc209a37d7bb973a2efa6424e77c794be
pratiksamarth <pratiksamarth296@gmail.com>
2019-05-29 16:44:01 +0530

    category.js committed

--- product.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-
-router.post('/product', (request, response) => {
-        response.send(utils.createResponse(error, products));
-});
-
-module.exports = router;
